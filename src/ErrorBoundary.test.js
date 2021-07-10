a690a51a98c75d70d60ae82dbc93fcf04b494a91
Seito Tanaka <seito.tanaka.dev@gmail.com>
2021-07-10 13:40:31 +0900

    Initial commit

---
+++ src/ErrorBoundary.test.js
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react'
+import ErrorBoundary from './ErrorBoundary'
+
+test('should be render fallback page Error was thrown', () => {
+  const InvalidComponent = () => undefined
+  const screen = render(
+    <ErrorBoundary>
+      <InvalidComponent />
+    </ErrorBoundary>
+  )
+  expect(screen.getByText('Something Error Ooccurring')).toBeInTheDocument()
+})
